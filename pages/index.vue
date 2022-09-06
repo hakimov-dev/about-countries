2cce4def3dd76104d91aea34dde5cb0178835531
Muhammadamin <webhacker0100@gmail.com>
2022-09-06 23:16:18 +0500

    Added main and filter content

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1>Hello world!</h1>
+  <div class="container-xl px-[5%] pt-[4%]">
+    <div class="filter w-full flex items-center justify-between">
+      <input class="w-[12%] outline-none p-2 dark:bg-black rounded-[10px] border-gray-400 border-solid border-[1.5px] transition-all hover:border-gray-200 focus:border-gray-200" type="text" name="search" placeholder="Enter country to search...">
+      <select>
+        <option value="asia">Asia</option>
+        <option value="europe">Europe</option>
+      </select>
+    </div>
+  </div>
 </template>
 
 <script>
