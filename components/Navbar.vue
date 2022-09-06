138788b7d655ffa37971fb898e6fc18f0030c743
Muhammadamin <webhacker0100@gmail.com>
2022-09-06 16:23:45 +0500

    Update classes and formatting

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,36 +1,55 @@
 <template>
- <div class="nav container-xl bg-white shadow-xl flex items-center px-[10%] h-[10vh]">
-   <NuxtLink  to="/" class="logo flex items-center">
-     <img src="../assets/imgs/logo.png" alt="">
-     <h1 class="text-[22px] font-medium">Countries about</h1>
-   </NuxtLink>
-   <div class="additional">
-    <button class="dark-mode">
-     <svg class="h-10 w-10 text-black"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M16.2 4a9.03 9.03 0 1 0 3.9 12a6.5 6.5 0 1 1 -3.9 -12" /></svg>
-    </button>
-    <a href="https://github.com/hakimov-dev" target="_blank"
-      ><svg
-        xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
-        aria-hidden="true"
-        role="img"
-        width="42"
-        height="42"
-        preserveAspectRatio="xMidYMid meet"
-        viewBox="0 0 24 24"
-        class="w-[42px] h-[42px] text-gray-600 hover:text-gray-800 button--github"
-      >
-        <path
-          d="M12 2.247a10 10 0 0 0-3.162 19.487c.5.088.687-.212.687-.475c0-.237-.012-1.025-.012-1.862c-2.513.462-3.163-.613-3.363-1.175a3.636 3.636 0 0 0-1.025-1.413c-.35-.187-.85-.65-.013-.662a2.001 2.001 0 0 1 1.538 1.025a2.137 2.137 0 0 0 2.912.825a2.104 2.104 0 0 1 .638-1.338c-2.225-.25-4.55-1.112-4.55-4.937a3.892 3.892 0 0 1 1.025-2.688a3.594 3.594 0 0 1 .1-2.65s.837-.262 2.75 1.025a9.427 9.427 0 0 1 5 0c1.912-1.3 2.75-1.025 2.75-1.025a3.593 3.593 0 0 1 .1 2.65a3.869 3.869 0 0 1 1.025 2.688c0 3.837-2.338 4.687-4.563 4.937a2.368 2.368 0 0 1 .675 1.85c0 1.338-.012 2.413-.012 2.75c0 .263.187.575.687.475A10.005 10.005 0 0 0 12 2.247z"
-          fill="currentColor"
-        ></path></svg
-    ></a>
-   </div>
- </div>
+  <div
+    class="nav container-xl bg-white shadow-xl w-full flex justify-between items-center px-[5%] h-[10vh]"
+  >
+    <NuxtLink to="/" class="logo flex items-center">
+      <img
+        class="h-[50px] w-[80px] ml-[20px]"
+        src="../assets/imgs/logo.png"
+        alt=""
+      />
+      <h1 class="text-[22px] font-medium dark:text-white">Countries about</h1>
+    </NuxtLink>
+    <div class="additional flex items-center">
+      <button class="dark-mode">
+        <svg
+          class="w-[38px] h-[38px] transition-all text-gray-600"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" />
+          <path d="M16.2 4a9.03 9.03 0 1 0 3.9 12a6.5 6.5 0 1 1 -3.9 -12" />
+        </svg>
+      </button>
+      <a href="https://github.com/hakimov-dev" target="_blank"
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          aria-hidden="true"
+          role="img"
+          width="42"
+          height="42"
+          preserveAspectRatio="xMidYMid meet"
+          viewBox="0 0 24 24"
+          class="w-[42px] h-[42px] transition-all text-gray-600"
+        >
+          <path
+            d="M12 2.247a10 10 0 0 0-3.162 19.487c.5.088.687-.212.687-.475c0-.237-.012-1.025-.012-1.862c-2.513.462-3.163-.613-3.363-1.175a3.636 3.636 0 0 0-1.025-1.413c-.35-.187-.85-.65-.013-.662a2.001 2.001 0 0 1 1.538 1.025a2.137 2.137 0 0 0 2.912.825a2.104 2.104 0 0 1 .638-1.338c-2.225-.25-4.55-1.112-4.55-4.937a3.892 3.892 0 0 1 1.025-2.688a3.594 3.594 0 0 1 .1-2.65s.837-.262 2.75 1.025a9.427 9.427 0 0 1 5 0c1.912-1.3 2.75-1.025 2.75-1.025a3.593 3.593 0 0 1 .1 2.65a3.869 3.869 0 0 1 1.025 2.688c0 3.837-2.338 4.687-4.563 4.937a2.368 2.368 0 0 1 .675 1.85c0 1.338-.012 2.413-.012 2.75c0 .263.187.575.687.475A10.005 10.005 0 0 0 12 2.247z"
+            fill="currentColor"
+          ></path></svg
+      ></a>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'navbar-item',
+  name: 'NavbarItem'
 }
 </script>
