871e7ea36d46b3b6b6b6f669c04dfe152eb54ddd
Muhammadamin <webhacker0100@gmail.com>
2022-09-06 14:55:34 +0500

    Update import css

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@/assets/css/tailwind.css'
+    '@/assets/css/index.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
