66b07f456fe36d8203d316c88606f1edceead0d7
Muhammadamin <webhacker0100@gmail.com>
2022-09-08 18:40:31 +0500

    Update

--- store/index.js
+++ store/index.js
@@ -1,26 +1,26 @@
-export default{
-    state: {
-        allCountry: null,
-        searchInput: '',
-        selectRegion: '',
-        country: null
-    },
-    mutations: {
-        setAllCountry(state, data){
-         state.allCountry = data
-        }
-    },
-    actions: {
-       async allCountries(ctx, payload){
-          try{
-           const { data } = await this.$axios.get(process.env.API_URL + '/all')
-           
-           ctx.commit('setAllCountry', data)
-          }catch(error){
-            console.log(error)
-          }
-        }
-    },
-    getters: {},
-    modules: {}
+/*eslint-disable */
+export const state = () => ({
+  allCountry: null,
+  searchInput: '',
+  selectRegion: '',
+  country: null
+})
+
+export const getters = {}
+
+export const mutations = {
+  setAllCountry(state, data) {
+    state.allCountry = data
+  }
+}
+
+export const actions = {
+  async allCountries(ctx, payload) {
+    try {
+      const { data } = await this.$axios.get(process.env.API_URL + '/all')
+      ctx.commit('setAllCountry', data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
