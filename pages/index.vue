66ff1a14cc87f4075d04de98a335b12cef707e5d
Muhammadamin <webhacker0100@gmail.com>
2022-09-08 13:13:54 +0500

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-xl px-[5%] pt-[4%]">
     <div class="filter w-full flex items-center justify-between">
-      <input class="w-[20%] outline-none p-2 dark:bg-[#18181B] rounded-[10px] border-gray-400 border-solid border-[1.5px] transition-all hover:border-gray-200 focus:border-gray-200" type="text" name="search" placeholder="Enter country name to search...">
+      <input class="w-[23%] dark:text-white text-black font-medium outline-none p-2 dark:bg-[#18181B] rounded-[10px] border-gray-400 border-solid border-[1.5px] transition-all hover:border-gray-200 focus:border-gray-200" type="text" name="search" placeholder="Enter country name to search...">
       <select>
         <option value="asia">Asia</option>
         <option value="europe">Europe</option>
@@ -17,5 +17,13 @@ export default {
 </script>
 
 <style scoped>
-
+input{
+  box-sizing: border-box;
+  padding-left: 36px;
+  background-image: url('../assets/imgs/search.png');
+  background-size: 20px;
+  background-position-x: 10px;
+  background-position-y: 50%;
+  background-repeat: no-repeat;
+  }
 </style>
