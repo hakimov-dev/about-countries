235b9a92a87ac8c1ac021c03dc945c0fc5bf85f2
Muhammadamin <webhacker0100@gmail.com>
2022-09-08 13:20:09 +0500

    Added select

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,10 @@
   <div class="container-xl px-[5%] pt-[4%]">
     <div class="filter w-full flex items-center justify-between">
       <input class="w-[23%] dark:text-white text-black font-medium outline-none p-2 dark:bg-[#18181B] rounded-[10px] border-gray-400 border-solid border-[1.5px] transition-all hover:border-gray-200 focus:border-gray-200" type="text" name="search" placeholder="Enter country name to search...">
-      <select>
-        <option value="asia">Asia</option>
-        <option value="europe">Europe</option>
+      <select class="select select-bordered w-full max-w-xs">
+       <option disabled selected>Who shot first?</option>
+       <option>Han Solo</option>
+       <option>Greedo</option>
       </select>
     </div>
   </div>
