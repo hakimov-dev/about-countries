db0b5d39ce7fcc5917ca26e1cca5bbde87eed3a8
Muhammadamin <webhacker0100@gmail.com>
2022-09-08 13:51:04 +0500

    Register '@nuxtjs/dotenv' package

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    '@nuxtjs/dotenv',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
