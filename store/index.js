0224d8635d6afed265a745af23ad43a50d2f008d
Muhammadamin <webhacker0100@gmail.com>
2022-09-08 18:17:03 +0500

    Added console

--- store/index.js
+++ store/index.js
@@ -10,6 +10,7 @@ export default{
        async allCountries(){
           try{
            const countries = await this.$axios.get(process.env.API_URL + '/all')
+           console.log(countries)
           }catch(error){
             console.log(error)
           }
