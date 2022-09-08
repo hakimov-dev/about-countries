6523270bf3694bd714be127894dfd7ea01d507a0
Muhammadamin <webhacker0100@gmail.com>
2022-09-08 15:50:04 +0500

    Added main template in component

---
+++ components/CountryCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <NuxtLink to="/">
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Country name</h2>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">More about</button>
+        </div>
+      </div>
+    </div>
+  </NuxtLink>
+</template>
