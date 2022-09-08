c3bd8cb622da9ad7a36204683f41504b3604dec6
Muhammadamin <webhacker0100@gmail.com>
2022-09-08 21:33:22 +0500

    Error found and corrected

--- pages/country/_country.vue
+++ pages/country/_country.vue
@@ -39,7 +39,7 @@ export default {
 
     mounted() {
        if(this.$route.params.country !== undefined){
-        this.$store.dispatch('searchCity', this.$router.params.country)
+        this.$store.dispatch('searchCity', this.$route.params.country)
        }else{
         this.$router.go(-1)
        }
