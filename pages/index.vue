2d503f3e221784451c5cd79fa9986e9eea9fe379
Muhammadamin <webhacker0100@gmail.com>
2022-09-08 13:27:29 +0500

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container-xl px-[5%] pt-[4%]">
     <div class="filter w-full flex items-center justify-between">
-      <input class="w-[23%] dark:text-white text-black font-medium outline-none p-2 dark:bg-[#18181B] rounded-[10px] border-gray-400 border-solid border-[1.5px] transition-all hover:border-gray-200 focus:border-gray-200" type="text" name="search" placeholder="Enter country name to search...">
-      <select class="select select-bordered w-full max-w-xs">
-       <option disabled selected>Who shot first?</option>
+      <input class="w-[23%] input dark:bg-[#18181B] bg-white dark:text-white outline-none text-black font-medium rounded-[10px] border-gray-400 border-solid border-[1.5px] transition-all hover:border-gray-200 focus:border-gray-200" type="text" name="search" placeholder="Enter country name to search...">
+      <select class="select select-bordered dark:bg-[#18181B] bg-white dark:text-white outline-none text-black w-[20%] max-w-xs">
+       <option disabled selected>Select region</option>
        <option>Han Solo</option>
        <option>Greedo</option>
       </select>
