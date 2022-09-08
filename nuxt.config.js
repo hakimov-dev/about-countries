cea99863ca97f3194a4753f92fd920c14900f7b5
Muhammadamin <webhacker0100@gmail.com>
2022-09-08 18:10:51 +0500

    Added axios baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: process.env.baseURL,
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
