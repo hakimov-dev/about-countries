3c845c82a5ed7108fb67e7bb8f17b19276da35a5
Muhammadamin <webhacker0100@gmail.com>
2022-09-08 18:12:54 +0500

    Deleted baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.baseURL,
+    baseURL: '/',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
