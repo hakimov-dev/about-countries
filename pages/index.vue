018157e06e5fb8159733ef308725c40b2441491a
Muhammadamin <webhacker0100@gmail.com>
2022-09-08 12:54:31 +0500

    Update placeholder

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-xl px-[5%] pt-[4%]">
     <div class="filter w-full flex items-center justify-between">
-      <input class="w-[20%] outline-none p-2 dark:bg-[#18181B] rounded-[10px] border-gray-400 border-solid border-[1.5px] transition-all hover:border-gray-200 focus:border-gray-200" type="text" name="search" placeholder="Enter country to search...">
+      <input class="w-[20%] outline-none p-2 dark:bg-[#18181B] rounded-[10px] border-gray-400 border-solid border-[1.5px] transition-all hover:border-gray-200 focus:border-gray-200" type="text" name="search" placeholder="Enter country name to search...">
       <select>
         <option value="asia">Asia</option>
         <option value="europe">Europe</option>
